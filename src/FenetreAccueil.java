4533d45c952dfa85990ba0d89537158ff37309c1
Cyrille Brière <cyrille.briere@gmail.com>
2015-12-26 16:35:22 +0100

    Modification du design de la fenêtre d'accueil.
    Background -> gris foncé
    Les deux boutons se partagent la largeur de la fenêtre.

--- src/FenetreAccueil.java
+++ src/FenetreAccueil.java
@@ -1,54 +1,89 @@
-import java.awt.Component;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
-
+/**
+ * 
+ * @author BRIERE / CARDON
+ *
+ */
 public class FenetreAccueil extends JFrame implements ActionListener{
 	JButton boutonReservation = new JButton("Reservation");
 	JButton boutonAdministration = new JButton("Administration");
 	
 	public Connect connect;
-
+	
 	public FenetreAccueil(Connect connect){
 		super("Panneau d'accueil");
 		this.connect=connect;
 
-
-
-		boutonReservation.setPreferredSize(new Dimension(250,30));
-		boutonAdministration.setPreferredSize(new Dimension(250,30));
+		Color GrisFonce = new Color(0x222222);
+		Color BlancPale = new Color (0xCFBFAD);
+		
+		//Bordure blanche d'épaisseur 3
+		Border border = new LineBorder(BlancPale, 3);
+		// Regarder comment importer une police
+		Font font_bouton = new Font("Roboto", Font.PLAIN, 50);
+		
+		//Taille du bouton
+		boutonReservation.setPreferredSize(new Dimension(383,550));
+		//Fond transparent
+		boutonReservation.setOpaque(false);
+		boutonReservation.setContentAreaFilled(false);
+		//Changement couleur bordure
+		boutonReservation.setBorder(border);
+		//Changement Police
+		boutonReservation.setFont(font_bouton);
+		//Changement couleur Police
+		boutonReservation.setForeground(BlancPale);
+		
+		boutonAdministration.setPreferredSize(new Dimension(383,550));
+		boutonAdministration.setOpaque(false);
+		boutonAdministration.setContentAreaFilled(false);
+		boutonAdministration.setBorder(border);
+		boutonAdministration.setFont(font_bouton);
+		boutonAdministration.setForeground(BlancPale);
 		
 		boutonReservation.addActionListener(this);
 		boutonAdministration.addActionListener(this);
-		JPanel panel = new JPanel();
-		BoxLayout box = new BoxLayout(panel,BoxLayout.PAGE_AXIS);
-		panel.setLayout(box);
-		panel.add(Box.createVerticalGlue());
-		panel.add(boutonReservation);
-		boutonReservation.setAlignmentX(Component.CENTER_ALIGNMENT);
-		panel.setLayout(box);
-		panel.add(Box.createVerticalGlue());
-		panel.add(boutonAdministration);
-		boutonAdministration.setAlignmentX(Component.CENTER_ALIGNMENT);
-		panel.setLayout(box);
-		panel.add(Box.createVerticalGlue());
+		
+		JPanel panel = new JPanel();		
+		
+		//BoxLayout box = new BoxLayout(panel,BoxLayout.LINE_AXIS);
+		
+
+		//panel.setLayout(box);
+		//panel.add(Box.createHorizontalGlue());
+		panel.add(boutonReservation,BorderLayout.LINE_START);
+		//boutonReservation.setAlignmentX(Component.CENTER_ALIGNMENT);
+				
+		
+		//panel.setLayout(box);
+		//panel.add(Box.createHorizontalGlue());
+		panel.add(boutonAdministration,BorderLayout.LINE_END);
+		//boutonAdministration.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		//panel.setLayout(box);
+		//panel.add(Box.createVerticalGlue());
 	
 
+		// Fond du panel
+		// couleur : gris foncé
+		panel.setBackground(GrisFonce);
 
-
+		//Ajout du panel
 		this.getContentPane().add(panel);
-		setSize(350,200);
+		//taille de la fenêtre
+		setSize(800,600);
+		//La fenêtre n'est pas redimensionnable
+		setResizable(false);
 		setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
@@ -73,7 +108,7 @@ public class FenetreAccueil extends JFrame implements ActionListener{
 	public static void main(String [] args){
 		Connect connect = new Connect();
 		JFrame frame = new FenetreAccueil(connect);
-
-
+		
+		
 	}
 }
