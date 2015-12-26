6217136c091dbdaa859c9f4454618f9a9e2daeab
Cyrille Brière <cyrille.briere@gmail.com>
2015-12-26 23:15:49 +0100

    Modification du design de la fenêtre AjouterVille

--- src/FenetreAjouterVille.java
+++ src/FenetreAjouterVille.java
@@ -1,16 +1,28 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
-
+/**
+ * 
+ * @author BRIERE / CARDON
+ *
+ */
 public class FenetreAjouterVille extends JFrame{
 
 	JTextField ville = new JTextField();
@@ -25,39 +37,95 @@ public class FenetreAjouterVille extends JFrame{
 		super("Ajout d'une ville");
 		this.connect=connect;
 		
+		Color GrisFonce = new Color(0x222222);
+		Color BlancPale = new Color (0xCFBFAD);
+
+		//Bordure blanche d'épaisseur 3
+		Border border = new LineBorder(BlancPale, 3);
+		// Regarder comment importer une police
+		Font font_bouton = new Font("Roboto", Font.PLAIN, 24);
+		
+		//Fond transparent
+		bouttonAnnuler.setOpaque(false);
+		bouttonAnnuler.setContentAreaFilled(false);
+		//Changement couleur bordure
+		bouttonAnnuler.setBorder(border);
+		//Changement Police
+		bouttonAnnuler.setFont(font_bouton);
+		//Changement couleur Police
+		bouttonAnnuler.setForeground(BlancPale);
+		
+		//Fond transparent
+		bouttonValider.setOpaque(false);
+		bouttonValider.setContentAreaFilled(false);
+		//Changement couleur bordure
+		bouttonValider.setBorder(border);
+		//Changement Police
+		bouttonValider.setFont(font_bouton);
+		//Changement couleur Police
+		bouttonValider.setForeground(BlancPale);
+		
 		bouttonValider.addActionListener(new TraitementAjoutVille(this,this.connect));
 		bouttonAnnuler.addActionListener(new TraitementAjoutVille(this,this.connect));
 		
 		
 		ville.setPreferredSize(new Dimension(250,30));
 
-		JPanel panel2 = new JPanel();
-		panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 
 
 		
 		JPanel panel3 = new JPanel(new FlowLayout());
+		panel3.setLayout(new GridBagLayout());
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.gridy = 0;
+		JLabel v = new JLabel("Ville");
+		v.setFont(font_bouton);
+		v.setForeground(BlancPale);
+		// similaire à un margin-left : 25px;
+		v.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 25));
+		panel3.add(v,c);
+			
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 1;
+		c.gridy = 0;
+		panel3.add(ville,c);
+		
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 0.25;
+		c.weighty = 0.50;
+		c.gridwidth = 2;
+		c.gridx = 0;
+		c.gridy = 0;
+		panel.add(panel3,c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 0.25;
+		c.weighty = 0.15;
+		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		panel.add(bouttonValider,c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 0.25;
+		c.weighty = 0.15;
+		c.gridwidth = 1;
+		c.gridx = 1;
+		c.gridy = 1;
+		panel.add(bouttonAnnuler,c);
+		
 
+		// Fond du panel
+		// couleur : gris foncé
+		panel3.setBackground(GrisFonce);
+		panel.setBackground(GrisFonce);
 		
-		panel3.add(new JLabel("ville"));
-		panel3.add(ville);
-		
-		JPanel panel5 = new JPanel(new BorderLayout());
-		panel5.add(panel3,BorderLayout.NORTH);
-		
-		
-		
-		
-		
-		JPanel panel7 = new JPanel();
-		panel7.add(bouttonValider);
-		panel7.add(bouttonAnnuler);
-		
-		panel2.add(panel5, BorderLayout.NORTH);
-		panel2.add(panel7, BorderLayout.SOUTH);
-
-
-		this.getContentPane().add(panel2);
+		this.getContentPane().add(panel);
 		setSize(500,300);
 		setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
