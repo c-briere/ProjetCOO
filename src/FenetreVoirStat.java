604e365b865d77c70834f82b347a05c77039d01c
Cyrille Brière <cyrille.briere@gmail.com>
2016-01-03 23:59:52 +0100

    Fin du design pour la dernière partie : STATS

--- src/FenetreVoirStat.java
+++ src/FenetreVoirStat.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -12,6 +13,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.JTableHeader;
 
 public class FenetreVoirStat extends JFrame implements ActionListener {
 
@@ -28,6 +30,9 @@ public class FenetreVoirStat extends JFrame implements ActionListener {
 		this.trajet = trajet;
 		this.connect = connect;
 
+		Color GrisFonce = new Color(0x222222);
+		Color BlancPale = new Color (0xCFBFAD);
+		
 		boutonCancel = new JButton("Annuler");
 				
 		String villeNom = "";
@@ -36,7 +41,9 @@ public class FenetreVoirStat extends JFrame implements ActionListener {
 		}
 		
 		JLabel jlabel = new JLabel("Ville(s) les plus voyagées " + villeNom);
+		jlabel.setForeground(BlancPale);
 		JLabel jlabel2 = new JLabel("Nombres de voyageurs " + nbDeVoyageur);
+		jlabel2.setForeground(BlancPale);
 
 		JPanel panel1 = new JPanel();
 		panel1.setLayout(new BoxLayout(panel1, BoxLayout.PAGE_AXIS));
@@ -48,6 +55,10 @@ public class FenetreVoirStat extends JFrame implements ActionListener {
 		JTable tableau = new JTable(new TableTrajet(trajet));
 		tableau.setPreferredScrollableViewportSize(new Dimension(500, 200));
 		tableau.setFillsViewportHeight(true);
+        //Modification du HEADER du tableau
+        JTableHeader header = tableau.getTableHeader();
+        header.setBackground(GrisFonce);
+        header.setForeground(BlancPale);
 		JScrollPane scrollPane = new JScrollPane(tableau);
 		
 		JPanel panel2 = new JPanel();
@@ -61,9 +72,15 @@ public class FenetreVoirStat extends JFrame implements ActionListener {
 		
 		add(panel);
 		
+		// Fond du panel
+		// couleur : gris foncé
+		panel.setBackground(GrisFonce);
+		panel1.setBackground(GrisFonce);
+		panel2.setBackground(GrisFonce);
+		
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(550, 250);
+		this.setSize(800, 250);
 		setVisible(true);
 
 	}
